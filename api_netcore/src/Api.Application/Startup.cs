3a2419c997d7574a9cad31b82f42f332aaf55b81
Lincoln Ferreira Campos <link7-7@hotmail.com>
2019-11-02 17:26:29 -0300

    finalizando a configuração do swagger

--- api_netcore/src/Api.Application/Startup.cs
+++ api_netcore/src/Api.Application/Startup.cs
@@ -6,11 +6,13 @@ using Api.CrossCutting.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Application
 {
@@ -30,7 +32,25 @@ namespace Application
 
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
+            //Configuração do Swagger!
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                new Info
+                {
+                    Title = "API AspNetCore 2.2",
+                    Version = "v1",
+                    Description = "Exemplo de API REST criado com ASP.NET Core",
+                    Contact = new Contact
+                    {
+                        Name = "Lincoln Ferreira Campos",
+                        Url = "https://github.com/LincolnLink"
+                    }
+                });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +60,20 @@ namespace Application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Ativando middlewares paa uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API com AspNetCore 2.2");
+            });
+
+            // Redireciona o Link para o Swagger, quando acessar a rota principal
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
+
 
             app.UseMvc();
         }
